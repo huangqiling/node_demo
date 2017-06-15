87ce80fbb77ab6fe241297b26cf3099fefe658ee
kylin <huangql@shishike.com>
2017-06-15 16:09:08 +0800

    base

---
+++ app.js
@@ -0,0 +1,11 @@
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const controller = require("./controller");
+
+const app = new Koa();
+
+app.use(bodyParser());
+app.use(controller());
+
+app.listen(3000);
+console.log("app started at port 3000...");
