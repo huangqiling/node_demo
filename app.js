080344ef0296f314b9169669a914dc2984f793d7
kylin <huangql@shishike.com>
2017-06-16 16:02:47 +0800

    one

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import controller from "./controller";
+import staticFiles from "./static-files";
 
 const app = new Koa();
-
+app.use(staticFiles("/views/", __dirname + "/views"));
 app.use(bodyParser());
 app.use(controller());
 
